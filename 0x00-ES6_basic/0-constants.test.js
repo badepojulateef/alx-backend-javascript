0a4fca5b44ce49d167aad41c51a580a07a915783
Adepoju Lateef <badepojulateef@gmail.com>
2023-09-25 21:36:05 +0100

    redo task 0

--- 0x00-ES6_basic/0-constants.test.js
+++ 0x00-ES6_basic/0-constants.test.js
@@ -1,5 +1,23 @@
-import { taskFirst, taskNext } from './0-constants.js';
+// eslint-disable-next-line import/extensions
+import { getLast, taskFirst, taskNext } from './0-constants.js';
 
-test('carry out first task', () => {
-  expect(taskFirst()).toBe('I prefer const when I can.');
+describe('task 0, case 0', () => {
+  it('carry out first task', () => {
+    expect.assertions(1);
+    expect(taskFirst()).toBe('I prefer const when I can.');
+  });
+});
+
+describe('task 0, case 1', () => {
+  it('carry out another task', () => {
+    expect.assertions(1);
+    expect(getLast()).toBe(' is okay');
+  });
+});
+
+describe('task 0, case 2', () => {
+  it('carry out first task', () => {
+    expect.assertions(1);
+    expect(taskNext()).toBe('But sometimes let is okay');
+  });
 });
