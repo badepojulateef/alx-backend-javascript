4c5314c2a7611d6d776c1006879c7f02d8086b9d
Adepoju Lateef <badepojulateef@gmail.com>
2023-09-26 05:39:11 +0100

    completed tasks

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,12 +1,27 @@
+/**
+ * Returns a constant task message.
+ *
+ * @returns {string} A constant task message.
+ */
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
+/**
+ * Returns the suffix " is okay."
+ *
+ * @returns {string} The suffix " is okay."
+ */
 export function getLast() {
   return ' is okay';
 }
 
+/**
+ * Combines a dynamic message with the suffix from `getLast`.
+ *
+ * @returns {string} The combined message.
+ */
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
