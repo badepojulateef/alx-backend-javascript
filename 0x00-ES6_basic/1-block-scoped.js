e2b8bc41256c12e9ca85ec23de14483101ee4e33
Adepoju Lateef <badepojulateef@gmail.com>
2023-09-26 04:22:50 +0100

    redo task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,23 @@
+/**
+ * This function takes a boolean value and returns an array with two boolean values.
+ * If the input `trueOrFalse` is true, it sets the first value to true and the second
+ * value to false. Otherwise, it sets both values to their default values (false).
+ *
+ * @param {boolean} trueOrFalse - A boolean value that determines the state of the returned values.
+ * @returns {Array} An array containing two boolean values.
+ */
 export default function taskBlock(trueOrFalse) {
-  let task = false; // Change 'var' to 'let'
-  let task2 = true; // Change 'var' to 'let'
+  // Initialize two boolean variables with default values.
+  let task = false;
+  let task2 = true;
 
+  // Check the input value `trueOrFalse`.
   if (trueOrFalse) {
-    task = true; // No need to redeclare, just assign the value
-    task2 = false; // No need to redeclare, just assign the value
+    // If `trueOrFalse` is true, update the values accordingly.
+    task = true;
+    task2 = false;
   }
 
+  // Return an array with the boolean values.
   return [task, task2];
 }
