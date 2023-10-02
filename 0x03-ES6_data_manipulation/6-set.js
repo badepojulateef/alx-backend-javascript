f319b9c6b6b50e73b772f330325aa8581e3e349b
Adepoju Lateef <badepojulateef@gmail.com>
2023-10-02 09:01:14 +0100

    added task 6

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,10 @@
+/**
+ * Create a Set from an array.
+ * @param {Array} array - The array from which to create a Set.
+ * @returns {Set} A Set containing the elements of the array.
+ */
+function setFromArray(array) {
+  return new Set(array);
+}
+
+export default setFromArray;
