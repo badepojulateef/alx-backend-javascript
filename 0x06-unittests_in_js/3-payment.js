7742ff711e513de7666c577f960c66973edfcbe8
Adepoju Lateef <badepojulateef@gmail.com>
2023-11-24 03:44:58 +0100

    updated task 3

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,16 +1,16 @@
 /**
  * This is a function, an API, that is calling the function defined in the Util module
  */
-const Utils = require('./utils')
+const Utils = require("./utils");
 
 /**
  * Call calculateNumber function/method in the Util module
  * @param {number} totalAmount
  * @param {number} totalShipping
  */
-const sendPaymentRequestToApi = function (totalAmount, totalShipping) {
-  const res = Utils.calculateNumber('SUM', totalAmount, totalShipping)
-  console.log(`The total is :${res}`)
-}
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+	const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
+	console.log(`The total is: ${result}`);
+};
 
-module.exports = sendPaymentRequestToApi
+module.exports = sendPaymentRequestToApi;
